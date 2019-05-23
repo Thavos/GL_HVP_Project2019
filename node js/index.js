c6fc19a0499399104bb9e5ab44a573bd840c0f91
THAVOS\Simon <simon.hruz252@gmail.com>
2019-05-23 10:15:03 +0200

    test

--- node js/index.js
+++ node js/index.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-const app = express();
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .get('/post', (req, res) => res.send({some : "json"}))
+  .get('/get', (req, res) => res.send({some : "json"}))
+  .get('/post', (req, res) => res.write({some : "json"}))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 //uselles coment
