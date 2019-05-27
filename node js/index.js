5ef3b55d7ac48bdb0a1945942db6628ff6e0d833
THAVOS\Simon <simon.hruz252@gmail.com>
2019-05-27 10:38:58 +0200

    mongo db added

--- node js/index.js
+++ node js/index.js
@@ -1,6 +1,9 @@
+const mongo = require('mongodb')
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+
+let mongoUrl = "mongodb+srv://gluser:<password>@glcluster-nzu7u.mongodb.net/test?retryWrites=true"
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -10,6 +13,25 @@ express()
 
   .get('/get', function(req, res){
     res.send({some : "other"});
+  })
+
+  .get('/db',function(req, res){
+    mongo.connect(mongoUrl, function(err, db){
+      var console = db.collection('users')
+      collection.findOne({
+          'basicInfo.firstName' : userName
+        },
+        function(err,result){
+          if(err){
+            console.err('Error');
+            res.status(500).send('Error');
+          }else{
+            console.log('Success')
+            res.status(200).json(result)
+          }
+        }  
+      )
+    })
   })
 
   .post('/post', function(req, res){
