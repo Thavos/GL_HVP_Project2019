cf642fb83d350f78ac90f6fc65988a9140386fe9
THAVOS\Simon <simon.hruz252@gmail.com>
2019-05-27 10:56:48 +0200

    test

--- node js/index.js
+++ node js/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-let mongoUrl = "mongodb+srv://glcluster:tajneheslo@glcluster-nzu7u.mongodb.net/test?retryWrites=true"
+let mongoUrl = "mongodb+srv://gluser:tajneheslo@glcluster-nzu7u.mongodb.net/test?retryWrites=true"
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
