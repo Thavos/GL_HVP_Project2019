a29489fc014151614f7533426414a0d8adb658e4
THAVOS\Simon <simon.hruz252@gmail.com>
2019-05-28 12:04:57 +0200

    123

--- node js/views/pages/database.js
+++ node js/views/pages/database.js
@@ -1,9 +1,9 @@
-const fs = require('fs')
+import { readFile } from 'fs';
 
 items = []
 
-function LoadData(){
-    fs.readFile('./data.json', 'utf8', function readFileCallback(err, data){
+new function LoadData(){
+    readFile('../../data.json', 'utf8', function(err, data){
         if (err){
             console.log(err);
         } else {
