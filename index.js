d5ffa45e172cf15f254df0ebc9775520f8db27d7
THAVOS\Simon <simon.hruz252@gmail.com>
2019-05-29 13:23:56 +0200

    TEST

---
+++ index.js
@@ -0,0 +1,77 @@
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+//MOJE
+const fs =  require('fs')
+
+let items = []
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', function(req, res){
+    fs.readFile('./data.json', 'utf8', function(err, data){
+        if (err){
+          console.log(err);
+        } else {
+          items = data
+          res.render('pages/index', { item : items });
+        }
+      }
+    )
+  })
+  .get('/get', function(req, res){
+    fs.readFile('./data.json', 'utf8', function readFileCallback(err, data){
+      if (err){
+          console.log(err);
+      } else {
+        items = JSON.parse(data);
+        let newDate = new Date()
+        let month = newDate.getMonth()
+        if(month < 10){
+          month = "0" + month
+        }
+        
+        let date = newDate.getFullYear() + '-' + month + '-' + newDate.getDate()
+        let time = newDate.getHours() + ':' + newDate.getMinutes()
+        
+        items.push({Date : date, Time : time})
+        json = JSON.stringify(items); 
+        
+        fs.writeFile('./data.json', json, 'utf8', function(){
+          fs.readFile('./settings.json', 'utf8', function(err, data){
+            if(err){
+              console.log(err)
+            }else{
+              //ZLY IF STATMENT
+              if(time > data.from && time < data.to){
+                let timeToLight = JSON.parse(data).time
+              }
+              else{
+                timeToLight = 0
+              }
+            }
+            res.send({ Time : timeToLight })
+          })
+        })
+      }
+    })
+  })
+  .get('/data', function(req, res){
+    items = []
+
+    new function LoadData(){
+          fs.readFile('./data.json', 'utf8', function(err, data){
+          if (err){
+              console.log(err);
+          } else {
+            
+          items = JSON.parse(data);
+          res.send(items)
+          }
+      })
+    }
+  })
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
