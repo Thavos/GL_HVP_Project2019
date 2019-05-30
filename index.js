b4d28adfc3a1b2c3cf2ab04e4013317efbb391f7
THAVOS\Simon <simon.hruz252@gmail.com>
2019-05-30 10:57:10 +0200

    123

--- index.js
+++ index.js
@@ -88,12 +88,7 @@ express()
         settings = JSON.parse(data)
         //console.log(settings[0].time2)
         console.log(settings) 
-        let set = JSON.stringify(req.body)
-        set.time1[2] = '.'
-        set.time2[2] = '.'
-        set.time1 = parseFloat(set.time1)
-        set.time2 = parseFloat(set.time2)
-        set.delay = parseFloat(set.delay)
+        let set = JSON.stringify(req.body).toString()
         res.send(set)
         //res.redirect('/');
       }
